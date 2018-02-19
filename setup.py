184c38c30729bff3ba1430634adebb0bae0ffae0
Robin Deits <robin.deits@gmail.com>
2018-02-19 18:06:52 -0500

    0.0.3

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name="meshcat",
-    version="0.0.2",
+    version="0.0.3",
     description="WebGL-based visualizer for 3D geometries and scenes",
     url="https://github.com/rdeits/meshcat-python",
-    download_url="https://github.com/rdeits/meshcat-python/archive/v0.0.2.tar.gz",
+    download_url="https://github.com/rdeits/meshcat-python/archive/v0.0.3.tar.gz",
     author="Robin Deits",
     author_email="mail@robindeits.com",
     license="MIT",
