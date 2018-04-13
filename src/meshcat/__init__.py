57f58222e6b73849e572d8c10d7b7541235e7ff9
Robin Deits <robin.deits@gmail.com>
2018-04-13 17:06:24 -0400

    use new webpack-packaged meshcat viewer

--- src/meshcat/__init__.py
+++ src/meshcat/__init__.py
@@ -12,7 +12,6 @@ def viewer_assets_path():
     return os.path.abspath(
         os.path.join(
             os.path.dirname(__file__),
-            "viewer",
-            "static"
+            "viewer"
         )
     )
