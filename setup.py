ed338af6938a2a1a4ff6ccb9c0854d19789cdf3e
Robin Deits <robin.deits@gmail.com>
2019-02-24 19:07:11 -0500

    Bump version to 0.0.14

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import sys
 from setuptools import setup, find_packages
 
 setup(name="meshcat",
-    version="0.0.13",
+    version="0.0.14",
     description="WebGL-based visualizer for 3D geometries and scenes",
     url="https://github.com/rdeits/meshcat-python",
-    download_url="https://github.com/rdeits/meshcat-python/archive/v0.0.13.tar.gz",
+    download_url="https://github.com/rdeits/meshcat-python/archive/v0.0.14.tar.gz",
     author="Robin Deits",
     author_email="mail@robindeits.com",
     license="MIT",
