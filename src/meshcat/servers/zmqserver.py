8713fa07ac023d8b8cbba00673e158ee140317a1
Daniel Farrell <daniel.farrell@teraview.com>
2020-09-08 15:48:35 +0100

    Actually specify a platform system!

--- src/meshcat/servers/zmqserver.py
+++ src/meshcat/servers/zmqserver.py
@@ -378,7 +378,7 @@ def main():
 
     # Fix for ZMQ crash on Windows. This allows starting up the
     # meshcat server but example script which start their own fail
-    if sys.version_info >= (3, 8) and platform.system():
+    if sys.version_info >= (3, 8) and platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     
     parser = argparse.ArgumentParser(description="Serve the MeshCat HTML files and listen for ZeroMQ commands")
