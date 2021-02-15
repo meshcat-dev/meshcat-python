e25b1e61029990df7c4ca433471ffbe70ce241cd
Robin Deits <robin.deits@gmail.com>
2021-02-14 23:37:46 -0500

    bump patch version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import sys
 from setuptools import setup, find_packages
 
 setup(name="meshcat",
-    version="0.1.0",
+    version="0.1.1",
     description="WebGL-based visualizer for 3D geometries and scenes",
     url="https://github.com/rdeits/meshcat-python",
-    download_url="https://github.com/rdeits/meshcat-python/archive/v0.1.0.tar.gz",
+    download_url="https://github.com/rdeits/meshcat-python/archive/v0.1.1.tar.gz",
     author="Robin Deits",
     author_email="mail@robindeits.com",
     license="MIT",
