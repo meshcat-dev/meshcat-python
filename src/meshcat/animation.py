9804de1606b03ababdbc97bac82547ebbdaa00b1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-18 05:14:09 +0000

    Adding tarfile member sanitization to extractall()

--- src/meshcat/animation.py
+++ src/meshcat/animation.py
@@ -136,7 +136,26 @@ def convert_frames_to_video(tar_file_path, output_path="output.mp4", framerate=6
         raise ValueError("The output path {:s} already exists. To overwrite that file, you can pass overwrite=True to this function.".format(output_path))
     with tempfile.TemporaryDirectory() as tmp_dir:
         with tarfile.open(tar_file_path) as tar:
-            tar.extractall(tmp_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, tmp_dir)
         args = ["ffmpeg",
                 "-r", str(framerate),
                 "-i", r"%07d.png",
